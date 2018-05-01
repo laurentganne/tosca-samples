c3b47bc86b86998897130cb5b190b5d5aca484e8
Laurent Ganne <ganne.laurent@gmail.com>
2018-05-01 22:47:05 +0200

    Added graphite

---
+++ monitoring/service/types/impl/grafana/scripts/connect-graphana_to_graphite.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Ystia Forge
+# Copyright (C) 2018 Bull S. A. S. - Bull, Rue Jean Jaures, B.P.68, 78340, Les Clayes-sous-Bois, France.
+# Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+#
+echo "Connecting Grafana to Graphite"
