a8302301bb2518d4b493c81e7632996d69e78a66
Laurent Ganne <ganne.laurent@gmail.com>
2018-09-03 11:44:23 +0000

    Added some stderr output

--- iterations/component/scripts/create.sh
+++ iterations/component/scripts/create.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "Created component to iterate $ITERATIONS times, with a pause of $PAUSE_DURATION seconds after each iteration"
+(>&2 echo "Writing to stderr line 1")
+(>&2 echo "Writing to stderr line 2")
