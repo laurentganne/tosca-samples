21de88c2d44a3a4691c6bdcb0c8ed8abc3a11bed
Laurent Ganne <ganne.laurent@gmail.com>
2018-09-05 16:12:35 +0000

    Updated interations sample

--- iterations/component/scripts/create.sh
+++ iterations/component/scripts/create.sh
@@ -2,4 +2,7 @@
 
 echo "Created component to iterate $ITERATIONS times, with a pause of $PAUSE_DURATION seconds after each iteration"
 (>&2 echo "Writing to stderr line 1")
+sleep 1
 (>&2 echo "Writing to stderr line 2")
+sleep 1
+
--- iterations/component/scripts/iterate.sh
+++ iterations/component/scripts/iterate.sh
@@ -5,6 +5,11 @@ ITERATIONS_DONE=0
 while [ "$ITERATIONS_DONE" -lt  "$ITERATIONS" ]
 do
     ((ITERATIONS_DONE++))
+
     echo "$MESSAGE iteration $ITERATIONS_DONE"
+
+    if [ `expr $ITERATIONS_DONE % 4` == 0 ]; then
+        (>&2 echo "$MESSAGE on stderr iteration $ITERATIONS_DONE")
+    fi
     sleep $PAUSE_DURATION
 done
