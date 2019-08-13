3614bca7e48bf89f67cbf7d03581c65889d386ac
Laurent Ganne <ganne.laurent@gmail.com>
2019-08-13 12:24:16 +0000

    Updated iterations sample

--- iterations/component/scripts/create.sh
+++ iterations/component/scripts/create.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-(>&2 echo "Writing to stderr line 1")
-(>&2 echo "Writing to stderr line 2")
 echo "Created component to iterate $ITERATIONS times, with a pause of $PAUSE_DURATION seconds after each iteration"
 
